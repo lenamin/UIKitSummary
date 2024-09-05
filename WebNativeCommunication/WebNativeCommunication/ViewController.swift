05bae97969200e5a25505482833ca5f096b763e5
lenamin <lenaminn@gmail.com>
2024-09-05 15:46:37 +0900

    Native -> JS 로 웹뷰

--- WebNativeCommunication/WebNativeCommunication/ViewController.swift
+++ WebNativeCommunication/WebNativeCommunication/ViewController.swift
@@ -10,6 +10,8 @@ import WebKit
 
 class ViewController: UIViewController {
     
+    var webView: WKWebView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,15 +39,60 @@ class ViewController: UIViewController {
         
         // MARK: - webView 생성
         
-        var webView = WKWebView(frame: view.frame, configuration: webViewConfiguration)
+        let frame = CGRect(origin: view.frame.origin,
+                           size: CGSize(width: view.frame.width, height: view.frame.height / 3 * 2))
+        
+        webView = WKWebView(frame: frame, configuration: webViewConfiguration)
         
         //MARK: - request 만들기
-        
+        view.addSubview(webView!)
         guard let url = URL(string: "http://127.0.0.1:8080") else { return }
         let request = URLRequest(url: url)
-        webView.load(request)
+        webView?.load(request)
+    }
+    
+    // JS 코드에 있는 call_func2() 호출
+    @IBAction func action1(_ sender: Any) {
+        webView?.evaluateJavaScript("call_func1()")
+    }
+    
+    // JS 코드에 있는 call_func2(title) 호출
+    @IBAction func action2(_ sender: Any) {
+        let name = "에스파"
+        webView?.evaluateJavaScript("call_func2('\(name)')")
+    }
+    
+    @IBAction func action3(_ sender: Any) {
+        let info = ["phone": "010-123-1234", "email": "a@a.com", "name": "홍길동", "gender": "male"]
+        // JSON 을 일단 만든다
+        // 직렬화 한다 (serialization)
+        // 왜? -> 이기종이기에 같이 알 수 있는 것으로 보내는 것 (그게 JSON)
         
-        view.addSubview(webView)
+        do {
+            // throw를 던진다 -> 예외가 발생할 때 처리
+            // 주는 쪽이 예외처리를 할 수는 없다 -> 받는 쪽으로 예외를 던지는 것!
+            // throw 있으면 try - catch 반드시 해야 함
+            
+            // JS에 데이터를 보낼 준비를 한다.
+            let infoData = try JSONSerialization.data(withJSONObject: info) // serialization
+            // JSON serialization : JSON을 만들고, 그 JSON으로 data를 다시 만든다
+            
+            // 받는 타입이 어떤 것이든지 (Any) Data 타입으로 바꿔준다.
+            guard let encodedData =  String(data: infoData, encoding: .utf8) else { return }
+            
+            // encodedData를 보낸다
+            // JS의 call_func3 에서 받는다
+            webView?.evaluateJavaScript("call_func3('\(encodedData)')", completionHandler: { result, error in
+                if let error {
+                    return
+                }
+                guard let info = result as? [String:String] else { return }
+                print(info)
+            })
+        } catch {
+            // 예외가 발생했을 때
+            print("에러 발생!!!")
+        }
     }
 }
 
@@ -68,11 +115,10 @@ extension ViewController: WKScriptMessageHandler {
             
             let info = message.body as? [String:String]
             
-            // JS 객체를 보냈다. (name, email, phone, gender) 를 보낸것! -> 딕셔너리처럼 생각하면 됨 
+            // JS 객체를 보냈다. (name, email, phone, gender) 를 보낸것! -> 딕셔너리처럼 생각하면 됨
             if let info {
                 print(info["name"]!, info["gender"]!, info["email"]!)
             }
-            
         } else {
             print("else 입니당")
         }
