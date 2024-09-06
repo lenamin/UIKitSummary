055d40468fdb74722f6c1d0d93d43e20f3023d99
lenamin <lenaminn@gmail.com>
2024-09-06 16:05:57 +0900

    [FEAT] TableView에 데이터 가져오기 구현

--- BookFinder/BookFinder/BookTableViewController.swift
+++ BookFinder/BookFinder/BookTableViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 class BookTableViewController: UITableViewController {
 
     var page = 1
+    var documents: [[String:Any]]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchWithQuery("사랑", page: 1)
     }
 
     func searchWithQuery(_ query: String?, page: Int) {
@@ -45,11 +45,13 @@ class BookTableViewController: UITableViewController {
             guard let data else { return }
             
             do {
-                guard let rootObject = try JSONSerialization.jsonObject(with: data) as? [String: Any], let documents = rootObject["documents"] as? [[String:Any]] else { return }
-                let first = documents[0]
-                if let title = first["title"] as? String {
-                    print(title)
+                guard let rootObject = try JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+                self.documents = rootObject["documents"] as? [[String:Any]]
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
                 }
+                
             } catch {
                 print("JSON parsing error 발생!")
             }
@@ -61,13 +63,47 @@ class BookTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 0
+        1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        documents?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "book", for: indexPath)
         
-        return 0
+        guard let book = documents?[indexPath.row] else { return  cell }
+        let imageView = cell.viewWithTag(1) as? UIImageView
+        
+        let titleLabel = cell.viewWithTag(2) as? UILabel
+        titleLabel?.text = book["title"] as? String
+        
+        let authorLabel = cell.viewWithTag(3) as? UILabel
+        let authors = book["authors"] as? [String]
+        authorLabel?.text = authors?.joined(separator: ", ")
+        
+        let publisherLabel = cell.viewWithTag(4) as? UILabel
+        publisherLabel?.text = book["publisher"] as? String
+        
+        let priceLabel = cell.viewWithTag(5) as? UILabel
+        priceLabel?.text = "\((book["price"] as? Int) ?? 0)원"
+        
+        if let thumbnail = book["thumbnail"] as? String {
+            if let url = URL(string: thumbnail) {
+                print("url: \(url)")
+                let request = URLRequest(url: url)
+                let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                    if let data {
+                        DispatchQueue.main.async {
+                            imageView?.image = UIImage(data: data)
+                        }
+                    }
+                }
+                task.resume()
+            }
+        }
+        return cell
     }
 }
 
